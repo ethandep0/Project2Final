7599c0f19964dfa27e84873aa6f9d6591620008c
ttbrowne <ttbrowne@iu.edu>
2023-09-13 17:14:36 -0400

    redo commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Project Name
+
+Description of the project ...
+
+## Functionality 
+
+The following **required** functionality is completed:
+
+* [ ] User sees 16 buttons sized dynamically to fit all Android screen resolutions along with a TextView displaying the output.
+* [ ] Upon changing the orientation of the device to landscape, you can see 5 additional buttons/operators (sin, cos, tan, Log 10, ln)
+* [ ] Whenever the orientation is changed, the app saves your inputs so no data is lost.
+* [ ] All buttons clicked are logged in the console using LogCat
+* [ ] User can also press operators like +, -, *, / to add, subtract, multiply, divide integers and floats alike (i.e. 6, 3.15, etc.)
+* [ ] There is also operators such as +/- which makes the number positive/negative as well as % which divides the number by 100.
+* [ ] Lastly, there is a clear button so the user can restart/change their calculations at any time.
+* [ ] The app also has a calculator icon.
+
+
+The following **extensions** are implemented:
+
+* View Finding 
+* String Resources
+* OnClickListener
+* Bundle (Variable saving)
+* LogCat
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='Walkthrough.gif' title='Video Walkthrough' width='50%' alt='Video Walkthrough'/>
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+When I was trying to add the horizontal buttons (sin, cos, tan, etc.) I wasn't sure how to add click listeners since the calculator is not always going to be in horizontal orientation. I figured out that you can check the orientation onCreate which, if it is indeed horizontal, I could then setup the click listeners for the respective buttons.
+
+It was easy to figure out how to utilize bundle to save my variables whenever the orientation is changed so the user wouldn't lose their calculation(s).
+
+## License
+
+    Copyright [2023] [Tip Browne, Ethan Deporter]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.project1
 
 import android.annotation.SuppressLint
 import android.content.res.ColorStateList
+import android.content.res.Configuration
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Button
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     //PARTNER: Ethan Deporter
 
     val DEFAULT_INPUT = "0"
+
     var firstNumber = DEFAULT_INPUT //first number in calculation
     var secondNumber = DEFAULT_INPUT //second number in calculation
     var operatorPressed = false //keep track of when operator pressed
@@ -31,9 +34,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val displayTextView: TextView =
-            findViewById(R.id.calculatorText) //calculator display text variable
-        displayTextView.text = DEFAULT_INPUT //default number shown on calculator (0)
+        val displayTextView: TextView = findViewById(R.id.calculatorText) //calculator display text variable
+
+        if (savedInstanceState != null) {
+            firstNumber = savedInstanceState.getString("firstNumber").toString()
+            secondNumber = savedInstanceState.getString("secondNumber").toString()
+            operatorPressed = savedInstanceState.getBoolean("operatorPressed")
+            operator = savedInstanceState.getString("operator", "")
+            equalsNumber = savedInstanceState.getString("equalsNumber", "")
+            equalsPressed = savedInstanceState.getBoolean("equalsPressed")
+
+            if (secondNumber != DEFAULT_INPUT) {
+                displayTextView.text = secondNumber
+            } else {
+                displayTextView.text = firstNumber
+            }
+
+        } else {
+            displayTextView.text = DEFAULT_INPUT //default number shown on calculator (0)
+        }
+
+
+
 
         fun executeCalculation() { //given firstNumber, operator, secondNumber, do math and set number on screen. // result of calculation becomes firstNumber
             var result = ""
@@ -43,19 +65,19 @@ class MainActivity : AppCompatActivity() {
             }
 
             if (operator == "/") {
-                result = (firstNumber.toFloat()/secondNumber.toFloat()).toString()
+                result = (firstNumber.toFloat() / secondNumber.toFloat()).toString()
             }
 
             if (operator == "*") {
-                result = (firstNumber.toFloat()*secondNumber.toFloat()).toString()
+                result = (firstNumber.toFloat() * secondNumber.toFloat()).toString()
             }
 
             if (operator == "+") {
-                result = (firstNumber.toFloat()+secondNumber.toFloat()).toString()
+                result = (firstNumber.toFloat() + secondNumber.toFloat()).toString()
             }
 
             if (operator == "-") {
-                result = (firstNumber.toFloat()-secondNumber.toFloat()).toString()
+                result = (firstNumber.toFloat() - secondNumber.toFloat()).toString()
             }
 
             //converts number back to integer if possible
@@ -69,17 +91,6 @@ class MainActivity : AppCompatActivity() {
             firstNumber = result
             displayTextView.text = result
             operatorPressed = false
-        }
-
-        //array of the operator ids to iterate over to ensure all are not pressed.
-        val operatorIds = arrayOf(
-            R.id.divideButton, R.id.multiplyButton, R.id.subtractButton, R.id.addButton
-        )
-
-        fun resetOperators() { //resets press state of operators for clear button or when another operator is pressed
-            for (button in operatorIds) {
-                val buttonObj: Button = findViewById(button)
-            }
         }
 
         //buttons 0-9
@@ -111,6 +122,8 @@ class MainActivity : AppCompatActivity() {
                     else -> throw IllegalArgumentException("Unsupported string value")
                 }
 
+                Log.d("Button Pressed", "'$strValue' button pressed.")
+
                 //clear and start new expression.
 
                 /* accounts for edge case described where
@@ -124,28 +137,29 @@ class MainActivity : AppCompatActivity() {
                     displayTextView.text = firstNumber
 
                     equalsPressed = false
-                }
-                else {
-                if (operatorPressed) { //operator pressed down, go to second number
-                    if (secondNumber == DEFAULT_INPUT && strValue != "0") { //first number pressed in sequence
-                        displayTextView.text = strValue
-                        secondNumber = strValue
-                    } else { //already numbers in, just add them
-                        val currentText = displayTextView.text.toString() //current text displayed
-                        displayTextView.text = "$currentText$strValue"
-                        secondNumber += strValue
+                } else {
+                    if (operatorPressed) { //operator pressed down, go to second number
+                        if (secondNumber == DEFAULT_INPUT && strValue != "0") { //first number pressed in sequence
+                            displayTextView.text = strValue
+                            secondNumber = strValue
+                        } else { //already numbers in, just add them
+                            val currentText =
+                                displayTextView.text.toString() //current text displayed
+                            displayTextView.text = "$currentText$strValue"
+                            secondNumber += strValue
+                        }
+                    } else { //operator not pressed, add to first number
+                        if (firstNumber == DEFAULT_INPUT && strValue != "0") { //first number pressed in sequence
+                            displayTextView.text = strValue
+                            firstNumber = strValue
+                        } else { //already numbers in, just add them
+                            val currentText =
+                                displayTextView.text.toString() //current text displayed
+                            displayTextView.text = "$currentText$strValue"
+                            firstNumber += strValue
+                        }
                     }
-                } else { //operator not pressed, add to first number
-                    if (firstNumber == DEFAULT_INPUT && strValue != "0") { //first number pressed in sequence
-                        displayTextView.text = strValue
-                        firstNumber = strValue
-                    } else { //already numbers in, just add them
-                        val currentText = displayTextView.text.toString() //current text displayed
-                        displayTextView.text = "$currentText$strValue"
-                        firstNumber += strValue
-                    }
                 }
-            }
 
 
             }
@@ -154,6 +168,8 @@ class MainActivity : AppCompatActivity() {
         //decimal button
         val decimalButton: Button = findViewById(R.id.decimalButton)
         decimalButton.setOnClickListener {
+            Log.d("Button Pressed", "'.' button pressed.")
+
             equalsNumber = DEFAULT_INPUT
 
             //clear and start new expression.
@@ -196,6 +212,7 @@ class MainActivity : AppCompatActivity() {
         //clear button
         val clearButton: Button = findViewById(R.id.clearButton)
         clearButton.setOnClickListener {
+            Log.d("Button Pressed","'C' button pressed.")
             //make everything default
             firstNumber = DEFAULT_INPUT
             secondNumber = DEFAULT_INPUT
@@ -204,29 +221,27 @@ class MainActivity : AppCompatActivity() {
             operatorPressed = false
             equalsPressed = false
             displayTextView.text = DEFAULT_INPUT
-
-            //TODO: RESET BUTTON PRESS STATES FUNCTION
         }
 
         // +/- button
         val plusMinusButton: Button = findViewById(R.id.negPosButton)
         plusMinusButton.setOnClickListener {
-            if (operatorPressed) { //2nd num
+            Log.d("Button Pressed","'+/-' button pressed.")
+
+            if (operatorPressed && secondNumber != DEFAULT_INPUT) { //2nd num
                 if ('-' !in secondNumber) {
                     secondNumber = "-$secondNumber"
                     displayTextView.text = secondNumber
-                }
-                else {
-                    secondNumber = secondNumber.replace("-","")
+                } else {
+                    secondNumber = secondNumber.replace("-", "")
                     displayTextView.text = secondNumber
                 }
             } else { //1st num
                 if ('-' !in firstNumber) {
                     firstNumber = "-$firstNumber"
                     displayTextView.text = firstNumber
-                }
-                else {
-                    firstNumber = firstNumber.replace("-","")
+                } else {
+                    firstNumber = firstNumber.replace("-", "")
                     displayTextView.text = firstNumber
                 }
             }
@@ -235,6 +250,8 @@ class MainActivity : AppCompatActivity() {
         // '/' - division button
         val divisionButton: Button = findViewById(R.id.divideButton)
         divisionButton.setOnClickListener {
+            Log.d("Button Pressed","'/' button pressed.")
+
             equalsNumber = DEFAULT_INPUT
             if (equalsPressed) { //edge case
                 equalsPressed = false
@@ -252,6 +269,8 @@ class MainActivity : AppCompatActivity() {
         // * - multiply button
         val multiplyButton: Button = findViewById(R.id.multiplyButton)
         multiplyButton.setOnClickListener {
+            Log.d("Button Pressed","'X' button pressed.")
+
             equalsNumber = DEFAULT_INPUT
             if (equalsPressed) { //edge case
                 equalsPressed = false
@@ -268,6 +287,8 @@ class MainActivity : AppCompatActivity() {
         // '-' - subtract button
         val subtractButton: Button = findViewById(R.id.subtractButton)
         subtractButton.setOnClickListener {
+            Log.d("Button Pressed","'-' button pressed.")
+
             equalsNumber = DEFAULT_INPUT
             if (equalsPressed) { //edge case
                 equalsPressed = false
@@ -284,6 +305,8 @@ class MainActivity : AppCompatActivity() {
         // '+' - addition button
         val addButton: Button = findViewById(R.id.addButton)
         addButton.setOnClickListener {
+            Log.d("Button Pressed","'+' button pressed.")
+
             equalsNumber = DEFAULT_INPUT
             if (equalsPressed) { //edge case
                 equalsPressed = false
@@ -300,12 +323,13 @@ class MainActivity : AppCompatActivity() {
         // % (divide current num by 100) button
         val percentButton: Button = findViewById(R.id.percentageButton)
         percentButton.setOnClickListener {
+            Log.d("Button Pressed","'%' button pressed.")
 
-            if (operatorPressed) { //2nd number
-                secondNumber = (secondNumber.toFloat()/100).toString()
+            if (operatorPressed && secondNumber != DEFAULT_INPUT) { //2nd number
+                secondNumber = (secondNumber.toFloat() / 100).toString()
                 displayTextView.text = secondNumber
             } else { //1st number
-                firstNumber = (firstNumber.toFloat()/100).toString()
+                firstNumber = (firstNumber.toFloat() / 100).toString()
                 displayTextView.text = firstNumber
             }
         }
@@ -313,6 +337,7 @@ class MainActivity : AppCompatActivity() {
         // = button
         val equalsButton: Button = findViewById(R.id.equalsButton)
         equalsButton.setOnClickListener {
+            Log.d("Button Pressed","'=' button pressed.")
             if (equalsNumber == DEFAULT_INPUT) { //saves input so you can keep pressing = if you want
                 equalsNumber = secondNumber
             }
@@ -321,14 +346,143 @@ class MainActivity : AppCompatActivity() {
                 secondNumber = firstNumber
                 executeCalculation()
                 operatorPressed = true
-            }
-            else {
+            } else {
                 executeCalculation()
             }
 
 
             equalsPressed = true
         }
+
+        /* Checks if the phone is in landscape mode. If so, initialize landscape only buttons (sin, cos, tan, etc.) */
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            //sin button
+            val sinButton: Button = findViewById(R.id.sinButton)
+            sinButton.setOnClickListener {
+                Log.d("Button Pressed","'sin' button pressed.")
+                if (operatorPressed && secondNumber != DEFAULT_INPUT) { //2nd number
+                    secondNumber = (kotlin.math.sin(secondNumber.toFloat()).toString())
+
+                    if (secondNumber.endsWith(".0")) { //change to int if needed.   
+                        secondNumber = secondNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = secondNumber
+                } else { //1st number
+                    firstNumber = (kotlin.math.sin(firstNumber.toFloat()).toString())
+
+                    if (firstNumber.endsWith(".0")) { //change to int if needed.
+                        firstNumber = firstNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = firstNumber
+                }
+            }
+
+            //cos button
+            val cosButton: Button = findViewById(R.id.cosButton)
+            cosButton.setOnClickListener {
+                Log.d("Button Pressed","'cos' button pressed.")
+                if (operatorPressed && secondNumber != DEFAULT_INPUT) { //2nd number
+                    secondNumber = (kotlin.math.cos(secondNumber.toFloat()).toString())
+
+                    if (secondNumber.endsWith(".0")) { //change to int if needed.
+                        secondNumber = secondNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = secondNumber
+
+                } else { //1st number
+                    firstNumber = (kotlin.math.cos(firstNumber.toFloat()).toString())
+
+                    if (firstNumber.endsWith(".0")) { //change to int if needed.
+                        firstNumber = firstNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = firstNumber
+                }
+            }
+
+            //tan button
+            val tanButton: Button = findViewById(R.id.tanButton)
+            tanButton.setOnClickListener {
+                Log.d("Button Pressed","'tan' button pressed.")
+                if (operatorPressed && secondNumber != DEFAULT_INPUT) { //2nd number
+                    secondNumber = (kotlin.math.tan(secondNumber.toFloat()).toString())
+
+                    if (secondNumber.endsWith(".0")) { //change to int if needed.
+                        secondNumber = secondNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = secondNumber
+
+                } else { //1st number
+                    firstNumber = (kotlin.math.tan(firstNumber.toFloat()).toString())
+
+                    if (firstNumber.endsWith(".0")) { //change to int if needed.
+                        firstNumber = firstNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = firstNumber
+
+                }
+            }
+
+            //Log 10 button
+            val log10Button: Button = findViewById(R.id.Log10Button)
+            log10Button.setOnClickListener {
+                Log.d("Button Pressed","'Log 10' button pressed.")
+                if (operatorPressed && secondNumber != DEFAULT_INPUT) { //2nd number
+                    secondNumber = (kotlin.math.log10(secondNumber.toFloat()).toString())
+
+                    if (secondNumber.endsWith(".0")) { //change to int if needed.
+                        secondNumber = secondNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = secondNumber
+
+                } else { //1st number
+                    firstNumber = (kotlin.math.log10(firstNumber.toFloat()).toString())
+
+                    if (firstNumber.endsWith(".0")) { //change to int if needed.
+                        firstNumber = firstNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = firstNumber
+                }
+            }
+
+            //ln button
+            val lnButton: Button = findViewById(R.id.lnButton)
+            lnButton.setOnClickListener {
+                Log.d("Button Pressed","'ln' button pressed.")
+                if (operatorPressed && secondNumber != DEFAULT_INPUT) { //2nd number
+                    secondNumber = kotlin.math.ln(secondNumber.toFloat()).toString()
+
+                    if (secondNumber.endsWith(".0")) { //change to int if needed.
+                        secondNumber = secondNumber.removeSuffix(".0")
+                    }
+                    displayTextView.text = secondNumber
+
+                } else { //1st number
+                    firstNumber = kotlin.math.ln(firstNumber.toFloat()).toString()
+
+                    if (firstNumber.endsWith(".0")) { //change to int if needed.
+                        firstNumber = firstNumber.removeSuffix(".0")
+
+                    }
+                    displayTextView.text = firstNumber
+                }
+            }
+        }
     }
+
+    /*
+    Variables that are saved when orientation is changed.
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("firstNumber", firstNumber)
+        outState.putString("secondNumber", secondNumber)
+        outState.putBoolean("operatorPressed", operatorPressed)
+        outState.putString("operator", operator)
+        outState.putString("equalsNumber", equalsNumber)
+        outState.putBoolean("equalsPressed", equalsPressed)
+    }
+
+
 }
 
